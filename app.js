6719722ab475c6bc8df204a55949b4b2f8affbfa
vickynocampoemail <you@example.com>
2022-05-25 20:34:49 -0300

    Cambios Clase 13

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const path = require("path");
+
 const app = express();
+
 const publicPath = path.resolve(__dirname, "./public");
 app.use(express.static(publicPath));
 
-app.get("/", (req, res)=> {res.sendFile(path.resolve(__dirname, "./views/home.html"))});
+app.get("/",(req, res)=>{res.sendFile(path.resolve(__dirname, "./views/home.html"))});
 
-app.listen(3030, ()=> {console.log("Servidor corriendo en puerto 3030");});
+app.listen(3030,()=>{console.log("Servidor corriendo en puerto 3030");})
