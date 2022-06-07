5f0ad797d056ef2f7bc8840d7798b395e76afc9d
vickynocampoemail <you@example.com>
2022-06-06 23:18:07 -0300

    clase 15

--- app.js
+++ app.js
@@ -7,5 +7,7 @@ const publicPath = path.resolve(__dirname, "./public");
 app.use(express.static(publicPath));
 
 app.get("/",(req, res)=>{res.sendFile(path.resolve(__dirname, "./views/home.html"))});
+app.get("/register",(req, res)=>{res.sendFile(path.resolve(__dirname, "./views/register.html"))});
+app.get("/login",(req, res)=>{res.sendFile(path.resolve(__dirname, "./views/login.html"))});
 
 app.listen(3030,()=>{console.log("Servidor corriendo en puerto 3030");})
