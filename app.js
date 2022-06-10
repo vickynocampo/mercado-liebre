fef8489e33fdf9a7c07baf435308b34041980190
vickynocampoemail <you@example.com>
2022-06-10 17:06:14 -0300

    install heroku

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const path = require("path");
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 const publicPath = path.resolve(__dirname, "./public");
 app.use(express.static(publicPath));
@@ -15,4 +16,4 @@ app.post("/register",(req, res)=>{res.redirect("/")});
 
 //app.listen(3000,()=>{console.log("Servidor corriendo en puerto 3000");})//
 
-app.listen(process.env.PORT || 3000 ,()=>{console.log("Servidor corriendo en puerto 3000");})
+app.listen((port) ,()=>{console.log("Servidor corriendo en puerto 3000");})
